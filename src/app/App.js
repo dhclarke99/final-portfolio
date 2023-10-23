575d667c2724e5d7fb817ed514548c24a58c4123
dhclarke99 <dhclarke99@gmail.com>
2023-10-23 14:26:44 -0400

    using hashrouter for reload debug

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   useLocation,
 } from "react-router-dom";
 import withRouter from "../hooks/withRouter";
@@ -20,7 +20,7 @@ const ScrollToTop = withRouter(_ScrollToTop);
 
 export default function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <ScrollToTop>
         <Headermain />
         <AppRoutes />
